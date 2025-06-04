61538386b18083b5659a7893fafdd24e47479ec2
Vitalijus Zimaitis <vitalijus.zimaitis@gmail.com>
2025-06-04 12:40:53 +0300

    chore(visits): chore schedule visits

--- src/app/rooms/actions.ts
+++ src/app/rooms/actions.ts
@@ -4,5 +4,10 @@ import { apiClient } from '@/app/actions';
 import { Rooms } from '@/types/swagger/RoomsRoute';
 
 export async function getRooms() {
-  return apiClient<Rooms.GetAllRooms.ResponseBody>('/rooms');
+  return apiClient<Rooms.GetAllRooms.ResponseBody>('/rooms?sector_id=1', {
+    next: {
+      revalidate: 3600, // Revalidate every hour
+      tags: ['rooms'],
+    },
+  });
 }
