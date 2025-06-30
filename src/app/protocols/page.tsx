9413f5a20f2a1f4a1e60406d333549729eb7e8be
Vitalijus Zimaitis <vitalijus.zimaitis@gmail.com>
2025-06-30 14:14:48 +0300

    chore(actions): Fixed incorrect old params removal

--- src/app/protocols/page.tsx
+++ src/app/protocols/page.tsx
@@ -1,15 +1,13 @@
-import { getMedicines } from '@/app/medicine/actions';
 import { getProtocols } from '@/app/protocols/actions';
 import { DashboardLayout } from '@/commons/layouts/DashboardLayout';
 import { ProtocolsList } from '@/features/protocols/ProtocolsList';
 
 export default async function Protocols() {
   const protocols = await getProtocols();
-  const medicines = await getMedicines();
 
   return (
     <DashboardLayout>
-      <ProtocolsList protocols={protocols} medicines={medicines} />
+      <ProtocolsList protocols={protocols} />
     </DashboardLayout>
   );
 }
