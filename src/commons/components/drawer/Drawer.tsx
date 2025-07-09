263b64716d93c427bb2c60bf2d76bb96eb78e848
Vitalijus Zimaitis <vitalijus.zimaitis@gmail.com>
2025-07-09 11:43:57 +0300

    chore(drawer): Made drawer only closable by X

--- src/commons/components/drawer/Drawer.tsx
+++ src/commons/components/drawer/Drawer.tsx
@@ -1,5 +1,7 @@
 import React, { Suspense } from 'react';
 
+import { X } from 'lucide-react';
+
 import { useActionContext } from '@/commons/action-context-provider/useActionContext';
 import {
   Drawer as BaseDrawer,
@@ -25,6 +27,7 @@ export const Drawer: React.FC<Props> = ({
 
   return (
     <BaseDrawer
+      dismissible={false}
       direction="right"
       open={isOpen}
       onClose={() => {
@@ -37,7 +40,21 @@ export const Drawer: React.FC<Props> = ({
     >
       <DrawerContent className="h-full overflow-y-auto overflow-x-hidden">
         <DrawerHeader>
-          <DrawerTitle>{title}</DrawerTitle>
+          <DrawerTitle className="flex justify-between items-center">
+            {title}
+            <button
+              className="text-gray-500 hover:text-gray-700 cursor-pointer"
+              onClick={() => {
+                if (typeof onClose !== 'undefined') {
+                  onClose();
+                } else {
+                  actionOnClose?.();
+                }
+              }}
+            >
+              <X />
+            </button>
+          </DrawerTitle>
         </DrawerHeader>
         <Suspense fallback="Loading...">{children}</Suspense>
       </DrawerContent>
